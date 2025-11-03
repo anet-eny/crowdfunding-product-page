8a4aca31a7c0f8f8424aa5889064aca82b34ca58
anet-eny <anet.eny@gmail.com>
2025-11-03 21:32:08 +0100

    Fix transition colors hover property at BookmarkIcon

--- src/components/icons/BookmarkIcon.jsx
+++ src/components/icons/BookmarkIcon.jsx
@@ -22,14 +22,20 @@ export default function BookmarkIcon({ variant = "inactive", size = 56 }) {
       height={size}
       viewBox="0 0 56 56"
       xmlns="http://www.w3.org/2000/svg"
-      className={`transition-colors duration-200 ${
+      className={
         variant === "inactive"
           ? "hover:[&>g>circle]:fill-[oklch(0.552_0.016_285.938)]"
           : ""
-      }`}
+      }
     >
       <g fill="none" fillRule="evenodd">
-        <circle fill={c.circle.base} cx="28" cy="28" r="28" />
+        <circle
+          fill={c.circle.base}
+          className="transition-colors duration-300"
+          cx="28"
+          cy="28"
+          r="28"
+        />
         <path fill={c.path.base} d="M23 19v18l5-5.058L33 37V19z" />
       </g>
     </svg>
