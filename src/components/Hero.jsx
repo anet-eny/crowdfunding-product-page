3afed34097372ecf436803d95c15fd4a7e4fd072
anet-eny <anet.eny@gmail.com>
2025-11-04 21:24:50 +0100

    Update layout of Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,17 +4,19 @@ import BookmarkButton from "./BookmarkButton";
 
 export default function Hero() {
   return (
-    <section className="relative flex flex-col items-center text-center max-w-3xl -mt-10 px-6 pt-12 pb-8 bg-white border border-gray-200 rounded-lg">
+    <section className="relative flex flex-col text-center max-w-3xl -mt-10 px-6 sm:px-12 pt-12 pb-8 bg-white border border-gray-200 rounded-lg">
       <img
         src={logo}
-        className="absolute -top-7 max-w-14"
+        className="absolute -top-7 max-w-14 self-center"
         alt="Mastercraft logo"
       />
-      <h1 className="text-preset-4 mb-4">Mastercraft Bamboo Monitor Riser</h1>
-      <p className="text-preset-8--regular text-gray-500 mb-8">
+      <h1 className="text-preset-4 sm:text-preset-2 mb-4">
+        Mastercraft Bamboo Monitor Riser
+      </h1>
+      <p className="text-preset-8--regular sm:text-preset-6--regular text-gray-500 mb-8">
         A beautifully handcrafted monitor stand to reduce neck and eye strain.
       </p>
-      <div>
+      <div className="flex justify-between gap-2 overflow-x-auto">
         <Button>Back this project</Button>
         <BookmarkButton />
       </div>
