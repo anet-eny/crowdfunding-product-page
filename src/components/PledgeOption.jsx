0e21eb30a3b646e26e28b9a69d0ea3816a47c1db
anet-eny <anet.eny@gmail.com>
2025-11-20 20:10:05 +0100

    Update desktop styles of PledgeOption

--- src/components/PledgeOption.jsx
+++ src/components/PledgeOption.jsx
@@ -29,18 +29,28 @@ export default function PledgeOption({
             onChange={onSelect}
             disabled={!available}
           />
-          <div className="sm:flex gap-4">
+          <div className="sm:flex gap-4 items-center flex-1">
             <h3 className="text-preset-8--bold">{pledge.title}</h3>
             <span className="text-preset-8--medium mt-2 sm:mt-0 text-teal-400">
               Pledge ${pledge.min} or more
             </span>
+            {pledge.remaining !== null && (
+              <div className="hidden sm:flex items-center gap-1.5 ml-auto">
+                <span className="text-preset-5--bold text-black">
+                  {pledge.remaining}
+                </span>
+                <span className="text-preset-7--regular text-gray-500">
+                  left
+                </span>
+              </div>
+            )}
           </div>
         </div>
         <p className="text-preset-8-regular text-gray-500 mt-4 px-6 sm:pl-13">
           {pledge.description}
         </p>
         {pledge.remaining !== null && (
-          <div className="flex items-center gap-1.5 mt-4 px-6">
+          <div className="flex items-center gap-1.5 mt-4 px-6 sm:hidden">
             <span className="text-preset-5--bold text-black">
               {pledge.remaining}
             </span>
