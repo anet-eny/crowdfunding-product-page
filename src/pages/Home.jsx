d28f16d7cc060bce5f1fd887c901e1436f9dedbc
anet-eny <anet.eny@gmail.com>
2025-11-24 11:02:23 +0100

    Use localStorage data from context in CampaignSummary

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,21 @@
+import { useContext } from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import CampaignSummary from "../components/CampaignSummary";
 import RewardList from "../components/RewardList";
-import { initialState } from "../data/initialState";
+import { CrowdfundingContext } from "../context/CrowdfundingContext";
 
 export default function Home() {
-  const { totalPledged, goal, backers, daysLeft } = initialState;
+  const { state: data } = useContext(CrowdfundingContext);
+
+  const { totalPledged, goal, backers, daysLeft } = data;
+
   return (
     <section>
       <Header />
       <main className="px-6 flex flex-col items-center gap-6">
         <Hero />
-        <CampaignSummary
-          totalPledged={totalPledged}
-          goal={goal}
-          backers={backers}
-          daysLeft={daysLeft}
-        />
+        <CampaignSummary {...{ totalPledged, goal, backers, daysLeft }} />
         <RewardList />
       </main>
     </section>
