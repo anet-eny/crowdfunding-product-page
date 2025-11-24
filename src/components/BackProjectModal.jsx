9bcb910d50c9c66f8d207757dc1b7f1a821a9868
anet-eny <anet.eny@gmail.com>
2025-11-24 10:14:57 +0100

    Add FocusTrap to modals

--- src/components/BackProjectModal.jsx
+++ src/components/BackProjectModal.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { CrowdfundingContext } from "../context/CrowdfundingContext";
 import iconCloseModal from "../assets/icon-close-modal.svg";
 import PledgeOption from "./PledgeOption";
+import { FocusTrap } from "focus-trap-react";
 
 export default function BackProjectModal({ isOpen, onClose, onSuccess }) {
   const {
@@ -37,39 +38,41 @@ export default function BackProjectModal({ isOpen, onClose, onSuccess }) {
       className="fixed inset-0 z-50 flex items-start justify-center p-6"
     >
       <div className="absolute inset-0 bg-black/30" onClick={onClose} />
-      <div className="relative z-10 w-full max-w-2xl mt-20 mx-auto bg-white rounded-lg shadow-2xl overflow-hidden">
-        <div className="flex flex-col text-left max-h-[calc(100vh-8rem)] p-6 sm:px-10 pt-4 sm:pt-6 overflow-y-auto">
-          <button
-            className="ml-auto -mr-2 sm:-mr-4 cursor-pointer"
-            aria-label="Close modal"
-            onClick={onClose}
-          >
-            <img src={iconCloseModal} alt="Close modal" />
-          </button>
-          <h2 className="text-preset-5--bold sm:text-preset-3 text-black">
-            Back this project
-          </h2>
-          <p className="mt-4 text-preset-8--regular sm:text-preset-6--regular text-gray-500">
-            Want to support us in bringing Mastercraft Bamboo Monitor Riser out
-            in the world?
-          </p>
-          <div className="flex flex-col gap-6 my-8">
-            {pledges.map((p, i) => (
-              <PledgeOption
-                key={p.id}
-                pledge={p}
-                isSelected={selectedId === p.id}
-                onSelect={() => handleSelect(p.id, p.min)}
-                firstRadioRef={i === 0 ? firstRadioRef : null}
-                amount={amount}
-                setAmount={setAmount}
-                onConfirm={onConfirmWrapper}
-                error={error}
-              />
-            ))}
+      <FocusTrap>
+        <div className="relative z-10 w-full max-w-2xl mt-20 mx-auto bg-white rounded-lg shadow-2xl overflow-hidden">
+          <div className="flex flex-col text-left max-h-[calc(100vh-8rem)] p-6 sm:px-10 pt-4 sm:pt-6 overflow-y-auto">
+            <button
+              className="ml-auto -mr-2 sm:-mr-4 cursor-pointer"
+              aria-label="Close modal"
+              onClick={onClose}
+            >
+              <img src={iconCloseModal} alt="Close modal" />
+            </button>
+            <h2 className="text-preset-5--bold sm:text-preset-3 text-black">
+              Back this project
+            </h2>
+            <p className="mt-4 text-preset-8--regular sm:text-preset-6--regular text-gray-500">
+              Want to support us in bringing Mastercraft Bamboo Monitor Riser
+              out in the world?
+            </p>
+            <div className="flex flex-col gap-6 my-8">
+              {pledges.map((p, i) => (
+                <PledgeOption
+                  key={p.id}
+                  pledge={p}
+                  isSelected={selectedId === p.id}
+                  onSelect={() => handleSelect(p.id, p.min)}
+                  firstRadioRef={i === 0 ? firstRadioRef : null}
+                  amount={amount}
+                  setAmount={setAmount}
+                  onConfirm={onConfirmWrapper}
+                  error={error}
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </FocusTrap>
     </div>
   );
 }
--- src/components/SuccessModal.jsx
+++ src/components/SuccessModal.jsx
@@ -1,8 +1,18 @@
+import { useRef } from "react";
 import iconCheck from "../assets/icon-check.svg";
 import Button from "./Button";
+import { FocusTrap } from "focus-trap-react";
 
 export default function SuccessModal({ isOpen = true, onClose }) {
   if (!isOpen) return null;
+
+  const containerRef = useRef(null);
+
+  const focusTrapOptions = {
+    initialFocus: () => containerRef.current,
+    clickOutsideDeactivates: true,
+  };
+
   return (
     <div
       role="dialog"
@@ -10,20 +20,26 @@ export default function SuccessModal({ isOpen = true, onClose }) {
       className="fixed inset-0 z-50 flex items-start justify-center p-6"
     >
       <div className="absolute inset-0 bg-black/30" onClick={onClose} />
-      <div className="relative flex flex-col items-center gap-8 z-10 w-full max-w-lg mt-20 mx-auto px-6 sm:px-10 py-8 bg-white rounded-lg shadow-2xl overflow-hidden">
-        <img src={iconCheck} alt="Checkmark icon" />
-        <h3 className="text-preset-5--bold text-black">
-          Thanks for your support!
-        </h3>
-        <p className="text-preset-8--regular text-gray-500">
-          Your pledge brings us one step closer to sharing Mastercraft Bamboo
-          Monitor Riser worldwide. You will get an email once our campaign is
-          completed.
-        </p>
-        <Button type="button" onClick={onClose}>
-          Got it!
-        </Button>
-      </div>
+      <FocusTrap focusTrapOptions={focusTrapOptions}>
+        <div
+          ref={containerRef}
+          className="relative flex flex-col items-center gap-8 z-10 w-full max-w-lg mt-20 mx-auto px-6 sm:px-10 py-8 bg-white rounded-lg shadow-2xl overflow-hidden"
+          tabIndex={-1}
+        >
+          <img src={iconCheck} alt="Checkmark icon" />
+          <h3 className="text-preset-5--bold text-black">
+            Thanks for your support!
+          </h3>
+          <p className="text-preset-8--regular text-gray-500">
+            Your pledge brings us one step closer to sharing Mastercraft Bamboo
+            Monitor Riser worldwide. You will get an email once our campaign is
+            completed.
+          </p>
+          <Button type="button" onClick={onClose}>
+            Got it!
+          </Button>
+        </div>
+      </FocusTrap>
     </div>
   );
 }
